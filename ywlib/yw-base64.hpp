6a9db5fb896c7c0e5fef6ac6d552af64006a2532
ywx9 <yw.ninefold@gmail.com>
2025-06-24 18:14:15 +0900

    base64 developing

---
+++ ywlib/yw-base64.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <ywlib>
+
+namespace yw {
+
+/// functor for `yw::base64`
+class Base64 {
+public:
+  /// decodes a Base64 encoded string
+  static array<char> decode(const str& Base64) {
+
+  }
+
+};
+}
